cac78a142765877c4e840e2ed6b51a910a507418
劉承峻 <jinccliu@gmail.com>
2026-06-04 11:46:08 +0800

    0604HW complete

---
+++ 0604/hw1/hw1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    while(true){
+        string input;
+        cout << "請輸入一行文字：";
+        getline(cin, input);
+
+        if(input[0] == '0')
+            break;
+
+        reverse(input.begin(), input.end());
+
+        cout << "反轉後的文字： " << input << endl;
+    }
+
+    return 0;
+}
---
+++ 0604/hw2/hw2.cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct TreeNode {
+    int value;
+    TreeNode* left;
+    TreeNode* right;
+    
+    TreeNode(int val) : value(val), left(nullptr), right(nullptr) {}
+};
+
+class BinarySearchTree {
+private:
+    TreeNode* root;
+    
+    TreeNode* insertNode(TreeNode* node, int value) {
+        if (node == nullptr) {
+            return new TreeNode(value);
+        }
+        
+        if (value < node->value) {
+            node->left = insertNode(node->left, value);
+        } else if (value > node->value) {
+            node->right = insertNode(node->right, value);
+        }
+        
+        return node;
+    }
+    
+    TreeNode* searchNode(TreeNode* node, int value) {
+        if (node == nullptr) {
+            return nullptr;
+        }
+        
+        if (value == node->value) {
+            return node;
+        } else if (value < node->value) {
+            return searchNode(node->left, value);
+        } else {
+            return searchNode(node->right, value);
+        }
+    }
+    
+public:
+    BinarySearchTree() : root(nullptr) {}
+    
+    void insert(int value) {
+        root = insertNode(root, value);
+    }
+    
+    TreeNode* search(int value) {
+        return searchNode(root, value);
+    }
+};
+
+int main() {
+    BinarySearchTree bst;
+    
+    // 第一行：輸入一組正整數
+    string line;
+    getline(cin, line);
+    
+    // 解析輸入的數字
+    int num;
+    size_t pos = 0;
+    while (pos < line.length()) {
+        // 跳過空格
+        while (pos < line.length() && isspace(line[pos])) {
+            pos++;
+        }
+        
+        if (pos < line.length()) {
+            int start = pos;
+            while (pos < line.length() && !isspace(line[pos])) {
+                pos++;
+            }
+            num = stoi(line.substr(start, pos - start));
+            bst.insert(num);
+        }
+    }
+    
+    // 第二行：輸入要搜索的數值
+    int searchValue;
+    cin >> searchValue;
+    
+    // 搜索並輸出結果
+    TreeNode* foundNode = bst.search(searchValue);
+    
+    if (foundNode != nullptr) {
+        // 輸出左右子節點的值
+        int leftVal = (foundNode->left != nullptr) ? foundNode->left->value : -1;
+        int rightVal = (foundNode->right != nullptr) ? foundNode->right->value : -1;
+        
+        cout << leftVal << " " << rightVal << endl;
+    } else {
+        cout << "The value is not in this binary search tree." << endl;
+    }
+    
+    return 0;
+}
